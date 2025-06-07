8e9712809407c4225c6e873ccf60381fd8208dc6
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2025-06-07 12:07:53 +0000

    feat(deploy): integrate WebsiteUpdateService into VercelService and update DeployModule imports

--- apps/api/src/deploy/deploy.module.ts
+++ apps/api/src/deploy/deploy.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DeployController } from './deploy.controller';
 import { VercelService } from './vercel.service';
 import { GitModule } from '../git/git.module';
+import { WebsiteGeneratorModule } from '../website-generator/website-generator.module';
 
 @Module({
-  imports: [GitModule],
-  providers: [VercelService],
-  controllers: [DeployController]
+    imports: [GitModule, WebsiteGeneratorModule],
+    providers: [VercelService],
+    controllers: [DeployController],
 })
 export class DeployModule {}
--- apps/api/src/deploy/vercel.service.ts
+++ apps/api/src/deploy/vercel.service.ts
@@ -1,59 +1,103 @@
-import { Injectable } from "@nestjs/common";
-import { DeployProvider, IDeployService, VercelInput } from "./deploy.types";
-import { GithubService } from "../git/github.service";
-import { Directory } from "../entities/directory.entity";
-import { User } from "../entities/user.entity";
+import { Injectable } from '@nestjs/common';
+import { DeployProvider, IDeployService, VercelInput } from './deploy.types';
+import { GithubService } from '../git/github.service';
+import { Directory } from '../entities/directory.entity';
+import { User } from '../entities/user.entity';
+import { WebsiteUpdateService } from '../website-generator/website-update.service';
 
 @Injectable()
 export class VercelService implements IDeployService {
     private readonly PROVIDER_ID: DeployProvider = 'vercel';
 
-    constructor(private readonly githubService: GithubService) {}
+    constructor(
+        private readonly githubService: GithubService,
+        private readonly websiteUpdateService: WebsiteUpdateService,
+    ) {}
 
     async deploy({ data, owner, repo }: VercelInput, directory: Directory, user: User) {
         const token = user.getGitToken();
         const publicKey = await this.githubService.repositoryPublickey(owner, repo, token);
 
         const promises = [
-            this.githubService.setActionVariable({
-                key: 'DEPLOY_PROVIDER',
-                value: this.PROVIDER_ID,
-                owner,
-                repo,
-            }, token),
-            this.githubService.setActionSecret({
-                key: 'DATA_REPOSITORY',
-                value: `${directory.slug}-data`,
-                owner,
-                repo,
-            }, publicKey, token),
-            this.githubService.setActionSecret({
-                key: 'VERCEL_TOKEN',
-                value: data.vercelToken,
-                owner,
-                repo,
-            }, publicKey, token)
+            this.githubService.setActionVariable(
+                {
+                    key: 'DEPLOY_PROVIDER',
+                    value: this.PROVIDER_ID,
+                    owner,
+                    repo,
+                },
+                token,
+            ),
+            this.githubService.setActionSecret(
+                {
+                    key: 'DATA_REPOSITORY',
+                    value: `${directory.slug}-data`,
+                    owner,
+                    repo,
+                },
+                publicKey,
+                token,
+            ),
+            this.githubService.setActionSecret(
+                {
+                    key: 'VERCEL_TOKEN',
+                    value: data.vercelToken,
+                    owner,
+                    repo,
+                },
+                publicKey,
+                token,
+            ),
         ];
 
         if (data.ghToken) {
-            promises.push(this.githubService.setActionSecret({
-                key: 'GH_TOKEN',
-                value: data.ghToken,
-                owner,
-                repo,
-            }, publicKey, token));
+            promises.push(
+                this.githubService.setActionSecret(
+                    {
+                        key: 'GH_TOKEN',
+                        value: data.ghToken,
+                        owner,
+                        repo,
+                    },
+                    publicKey,
+                    token,
+                ),
+            );
         }
 
         await Promise.all(promises);
 
-        await this.githubService.dispatchAction({
-            workflow: 'deploy_vercel.yaml',
-            inputs: {
-                environment: 'production',
+        const dispatchDeployAction = () => {
+            return this.githubService.dispatchAction(
+                {
+                    workflow: 'deploy_vercel.yaml',
+                    inputs: {
+                        environment: 'production',
+                    },
+                    branch: 'develop', // for now
+                    owner,
+                    repo,
+                },
+                token,
+            );
+        };
+
+        const tries = [
+            dispatchDeployAction,
+            async () => {
+                console.log('Trying to update repository instead');
+                await this.websiteUpdateService.updateRepository(directory, user);
+                await dispatchDeployAction();
             },
-            branch: 'develop',  // for now
-            owner,
-            repo,
-        }, token);
+        ];
+
+        for (const tryFn of tries) {
+            try {
+                await tryFn();
+                return;
+            } catch (error) {
+                console.warn('Failed to deploy:', error);
+            }
+        }
     }
 }
