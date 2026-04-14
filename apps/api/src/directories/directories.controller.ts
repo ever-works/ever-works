a491b30aae435cf73b5908602ae501006556b2e4
Paradoxe Ngwasi <paradoxngwasi@gmail.com>
2026-04-14 18:36:06 +0000

    Updates conditional logic formatting for readability

--- apps/api/src/directories/directories.controller.ts
+++ apps/api/src/directories/directories.controller.ts
@@ -552,7 +552,10 @@ export class DirectoriesController {
             user,
         );
 
-        if (updateScheduleDto.runImmediately && schedule.status === DirectoryScheduleStatus.ACTIVE) {
+        if (
+            updateScheduleDto.runImmediately &&
+            schedule.status === DirectoryScheduleStatus.ACTIVE
+        ) {
             const scheduleEntity = await this.directoryScheduleService.getScheduleEntity(id, user);
             await this.directoryGenerationService.runScheduledUpdate(scheduleEntity);
         }
